8d59843c2ac876c7f37c8ae4b5559b642eef1ae9
newave986 <kingofthefall538@gmail.com>
2021-09-20 04:51:58 +0900

    [Week 1] update homework answer : sqrt

--- WEEK_01/homework_answer/hw2.c
+++ WEEK_01/homework_answer/hw2.c
@@ -2,3 +2,12 @@
 // 구한 후, 그 값을 필드 폭 10칸 & 오른쪽 정렬 실수로 출력하는 프로그램을 작성하여라
 
 #include <stdio.h>
+#include <math.h>
+
+int main(void) {
+	int n;
+	printf_s("양의 정수 n을 입력하세요: ");
+	scanf_s("%d", &n);
+	printf_s("n의 제곱근값은: %10f", sqrt(n));
+	return 0;
+}
